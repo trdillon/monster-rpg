44588cf83610289796176175cf975c3c4c026f7c
Tim Dillon <tim@itsdits.com>
2021-02-26 16:15:04 +0900

    Add monsters (#12)
    *Add monster base class, type enum, implementation
    *Add 3 starter monsters without sprites
    *Organize Art directories

---
+++ Assets/Scripts/Monster/MonsterType.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonsterType
+{
+   None,
+   Normal,
+   Aqua,
+   Ember,
+   Earth,
+   Shock,
+   Air,
+   Spirit,
+   Force,
+   Shadow,
+   Light
+}
