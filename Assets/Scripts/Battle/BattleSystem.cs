beac082d99a5e53429f66d6ec901071274975847
Tim Dillon <tim@itsdits.com>
2021-02-28 11:26:01 +0900

    Add action and move selection in battle UI
    *Add action handlers
    *Add move selection dialog box
    *Add battle state enums
    **Add missing move list from monster bases
    **Fix missing name for Splash move

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -8,17 +8,109 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] BattleMonster enemyMonster;
     [SerializeField] BattleHUD playerHUD;
     [SerializeField] BattleHUD enemyHUD;
+    [SerializeField] BattleDialogBox dialogBox;
+    BattleState state;
+    int currentAction;
+    int currentMove;
 
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle()
+    private void Update()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    public IEnumerator SetupBattle()
     {
         playerMonster.Setup();
         enemyMonster.Setup();
         playerHUD.SetData(playerMonster.Monster);
         enemyHUD.SetData(enemyMonster.Monster);
+        dialogBox.SetMoveList(playerMonster.Monster.Moves);
+
+        yield return dialogBox.TypeDialog($"You have encountered an enemy {enemyMonster.Monster.Base.Name}!");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Select an action:"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+                ++currentAction;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                // Fight
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                // Run
+            }
+        }
+    }
+
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerMonster.Monster.Moves.Count - 1)
+                ++currentMove;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+                --currentMove;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerMonster.Monster.Moves.Count - 2)
+                currentMove += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+                currentMove -= 2;
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerMonster.Monster.Moves[currentMove]);
     }
 }
