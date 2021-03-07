aab0c0c7a362789dae3fd735a2500984058d3792
Tim Dillon <tim@itsdits.com>
2021-03-07 15:19:00 +0900

    Fix NPC doesn't look on interact (#84)
    *Add function to turn NPC towards character who initiates interaction

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -56,6 +56,18 @@ public class Character : MonoBehaviour
         animator.IsMoving = IsMoving;
     }
 
+    public void TurnToInteract(Vector3 targetPos)
+    {
+        var xdiff = Mathf.Floor(targetPos.x) - Mathf.Floor(transform.position.x);
+        var ydiff = Mathf.Floor(targetPos.y) - Mathf.Floor(transform.position.y);
+
+        if (xdiff == 0 | ydiff == 0)
+        {
+            animator.MoveX = Mathf.Clamp(xdiff, -1f, 1f);
+            animator.MoveY = Mathf.Clamp(ydiff, -1f, 1f);
+        }
+    }
+
     private bool IsWalkable(Vector3 targetPos)
     {
         if (Physics2D.OverlapCircle(targetPos, 0.1f, MapLayers.Instance.ObjectsLayer | MapLayers.Instance.InteractLayer) != null)
