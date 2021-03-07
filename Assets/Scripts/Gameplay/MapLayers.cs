55369b385f1d889bdb9c2045e5b489af3218067f
Tim Dillon <tim@itsdits.com>
2021-03-07 13:54:17 +0900

    Add NPC walk patterns (#77)
    *Add NPC walk pattern logic
    *Create Character superclass to handle common logic
    *Add MapLayer class to handle Layers logic
    **Change map layer names to be more descriptive

---
+++ Assets/Scripts/Gameplay/MapLayers.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLayers : MonoBehaviour
+{
+    [SerializeField] LayerMask objectsLayer;
+    [SerializeField] LayerMask encountersLayer;
+    [SerializeField] LayerMask interactLayer;
+
+    public static MapLayers Instance { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public LayerMask ObjectsLayer {
+        get => objectsLayer;
+    }
+    public LayerMask EncountersLayer {
+        get => encountersLayer;
+    }
+    public LayerMask InteractLayer {
+        get => interactLayer;
+    }
+}
