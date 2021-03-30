21c3f1402e76e3e969cd74928508ad98db31ec02
Tim Dillon <tim@itsdits.com>
2021-03-30 20:54:30 +0900

    Persist core objects between scenes (#133)
    *Add CorePack, Loader and Builder
    *Add Pause during scene switch
    *Fix duplicate character after scene switch
    *Add spawn points to portals
    * Add portalId
    *Rename System folders to Core to avoid keyword issues

---
+++ Assets/Ravar/Scripts/Levels/PortalID.cs
@@ -0,0 +1,15 @@
+namespace Itsdits.Ravar
+{
+    /// <summary>
+    /// Identifier to connect portals in multi-portal maps.
+    /// </summary>
+    public enum PortalID
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F
+    }
+}
