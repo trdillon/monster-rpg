4941b65d367574e1801894d64e2117bd9821ad6f
Tim Dillon <tim@itsdits.com>
2021-04-21 20:26:56 +0900

    Add Settings Menu (#158)
    * Add Settings Menu scene
    
    * Fix SaveSettings return to menu function

---
+++ Assets/Ravar/Scripts/UI/Menu/SettingsMenuController.cs
@@ -0,0 +1,50 @@
+using Itsdits.Ravar.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Itsdits.Ravar.UI.Menu
+{
+    /// <summary>
+    /// Controller class for the Settings Menu scene. <seealso cref="MenuController"/>
+    /// </summary>
+    public class SettingsMenuController : MenuController
+    {
+        [Header("UI Buttons")]
+        [Tooltip("Back button to return to the Main Menu.")]
+        [SerializeField] private Button _backButton;
+        [Tooltip("Save button to save the current settings.")]
+        [SerializeField] private Button _saveButton;
+        
+        private void OnEnable()
+        {
+            EnableSceneManagement();
+            _backButton.onClick.AddListener(ReturnToMenu);
+            _saveButton.onClick.AddListener(SaveSettings);
+        }
+
+        private void OnDisable()
+        {
+            _backButton.onClick.RemoveListener(ReturnToMenu);
+            _saveButton.onClick.RemoveListener(SaveSettings);
+        }
+        
+        //TODO - implement localization settings
+        
+        //TODO - implement audio settings
+
+        private void SaveSettings()
+        {
+            // Save settings
+            DisableSceneManagement();
+            string previousScene = PlayerPrefs.GetString("previousMenu");
+            StartCoroutine(SceneLoader.Instance.LoadScene(previousScene == "UI.Menu.Pause" ? 
+                                                              "UI.Menu.Pause" : "UI.Menu.Main"));
+        }
+
+        private void ReturnToMenu()
+        {
+            DisableSceneManagement();
+            StartCoroutine(SceneLoader.Instance.LoadScene("UI.Menu.Main"));
+        }
+    }
+}
