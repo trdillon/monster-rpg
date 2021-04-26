57a11fa9e89cea1ec37995bbcdb49d7d99ad5490
Tim Dillon <tim@itsdits.com>
2021-04-26 11:02:16 +0900

    Refactor Core Controller for new EventSystem handling

--- Assets/Ravar/Scripts/Core/CoreController.cs
+++ Assets/Ravar/Scripts/Core/CoreController.cs
@@ -2,34 +2,23 @@ using Itsdits.Ravar.Character;
 using Itsdits.Ravar.Core.Signal;
 using Itsdits.Ravar.Data;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Itsdits.Ravar.Core
 {
     /// <summary>
-    /// Controller class for the core game scene. Controls player, camera and event functions.
+    /// Controller class for the core game scene.
     /// </summary>
     public class CoreController : MonoBehaviour
     {
         [Header("Player")]
         [Tooltip("Controller object for the player.")]
         [SerializeField] private PlayerController _playerController;
-        [Tooltip("Camera attached to the player prefab.")]
-        [SerializeField] private Camera _playerCamera;
-
-        [Header("Event System")]
-        [Tooltip("Event System for the core scene.")]
-        [SerializeField] private EventSystem _eventSystem;
 
         private void Awake()
         {
-            DisablePlayer();
+            HidePlayer();
             GameSignals.GAME_NEW.AddListener(LoadGame);
             GameSignals.GAME_LOAD.AddListener(LoadGame);
-            GameSignals.GAME_PAUSE.AddListener(OnPause);
-            GameSignals.GAME_RESUME.AddListener(OnResume);
-            GameSignals.DIALOG_OPEN.AddListener(OnDialogOpen);
-            GameSignals.DIALOG_CLOSE.AddListener(OnDialogClose);
             GameSignals.BATTLE_START.AddListener(OnBattleStart);
         }
 
@@ -37,16 +26,12 @@ namespace Itsdits.Ravar.Core
         {
             GameSignals.GAME_NEW.RemoveListener(LoadGame);
             GameSignals.GAME_LOAD.RemoveListener(LoadGame);
-            GameSignals.GAME_PAUSE.RemoveListener(OnPause);
-            GameSignals.GAME_RESUME.RemoveListener(OnResume);
-            GameSignals.DIALOG_OPEN.RemoveListener(OnDialogOpen);
-            GameSignals.DIALOG_CLOSE.RemoveListener(OnDialogClose);
             GameSignals.BATTLE_START.RemoveListener(OnBattleStart);
         }
 
         private void LoadGame(string sceneName)
         {
-            EnablePlayer();
+            ShowPlayer();
             string sceneToLoad = GameData.PlayerData.currentScene;
             string previousScene = PlayerPrefs.GetString("previousMenu");
             if (previousScene == "UI.Menu.Main")
@@ -60,48 +45,24 @@ namespace Itsdits.Ravar.Core
             }
         }
         
-        private void EnablePlayer()
+        private void ShowPlayer()
         {
-            //_eventSystem.enabled = true;
-            //_playerCamera.enabled = true;
             _playerController.GetComponent<SpriteRenderer>().enabled = true;
         }
 
-        private void DisablePlayer()
+        private void HidePlayer()
         {
-            //_eventSystem.enabled = false;
-            //_playerCamera.enabled = false;
             _playerController.GetComponent<SpriteRenderer>().enabled = false;
-        }
-
-        private void OnPause(bool paused)
-        {
-            //_eventSystem.enabled = false;
-        }
-
-        private void OnResume(bool resumed)
-        {
-            //_eventSystem.enabled = true;
-        }
-
-        private void OnDialogOpen(DialogItem dialogItem)
-        {
-            //_eventSystem.enabled = false;
-        }
-
-        private void OnDialogClose(string speakerName)
-        {
-            //_eventSystem.enabled = true;
         }
 
         private void OnBattleStart(BattlerEncounter battler)
         {
-            DisablePlayer();
+            HidePlayer();
         }
         
         private void OnBattleFinish()
         {
-            EnablePlayer();
+            ShowPlayer();
         }
     }
 }
